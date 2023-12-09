95824ece576832fa00df07be568fe2a0e646437e
Raymond Chan <raymond_rc_chan@cathaypacific.com>
2023-12-09 23:58:19 +0800

    add accuracy func

--- hub/evaluator.py
+++ hub/evaluator.py
@@ -1,5 +1,13 @@
 import evaluate
+
+
 def frugalscore():
     frugalscore = evaluate.load("frugalscore", "moussaKam/frugalscore_medium_bert-base_mover-score")
     results = frugalscore.compute(predictions=['hello world'], references=['hugging face'])
+    return results
+
+
+def accuracy(references, predictions):
+    accuracy_metric = evaluate.load("accuracy")
+    results = accuracy_metric.compute(references=references, predictions=predictions)
     return results
